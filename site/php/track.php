bffdb1f1619384f81e69c8b0249a249b48fbd32a
qkfreas <qkfreas@gmail.com>
2017-06-30 05:27:10 -0400

    Add files via upload

---
+++ site/php/track.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="../pingendo-page/theme.css">
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: qkfre
+ * Date: 6/30/2017
+ * Time: 5:07 AM
+ */
+include ("../pingendo-page/header.html");
+include ("../pingendo-page/000015.html");
+include ("../pingendo-page/footer.html");
+?>
